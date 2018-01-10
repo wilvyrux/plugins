f58b159ad9787551a35bf61194db39155d993c7d
Jay Aries <jariesdev@gmail.com>
2018-01-10 19:09:05 +0800

    1st Shortcode

---
+++ wx-shortcodes.php
@@ -0,0 +1,23 @@
+<?php
+
+defined( 'ABSPATH' ) or die( 'No No NO!' );
+
+/*includes*/
+include 'shortcodes/wx_buttons.php';
+
+
+Class Wx_Shortcodes {
+
+	public domain = 'wx-shortcodes';
+	
+	function __construct (){
+
+	}
+
+	function register_settings(){
+
+	}
+
+}
+
+new Wx_Shortcodes();
