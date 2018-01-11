3818e5677cfcf65981d29ca22644c00c6088d688
Jay Aries <jariesdev@gmail.com>
2018-01-11 20:35:40 +0800

    updated

--- wx-shortcodes.php
+++ wx-shortcodes.php
@@ -1,9 +1,19 @@
 <?php
+/**
+ * @package WX_Shortcodes
+ * @version 1.0
+ */
+/*
+Plugin Name: WX Shortcodes
+Plugin URI: #
+Description: 
+Author: Spider Owls
+Version: 1.0
+Author URI: #
+*/
+
 
 defined( 'ABSPATH' ) or die( 'No No NO!' );
-
-/*includes*/
-
 
 Class Wx_Shortcodes {
 
@@ -15,7 +25,8 @@ Class Wx_Shortcodes {
 
 	function init(){
 		$enabled_shortcodes = array(
-			'wx_buttons'
+			'wx_buttons',
+			'wx_buttons',
 		);
 
 		foreach ($enabled_shortcodes as $v) {
