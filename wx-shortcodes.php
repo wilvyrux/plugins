79b7894b663a324995d956d4dc9ebc0e7d37c40c
jariesdev <jariesdev@gmail.com>
2018-01-30 18:31:45 +0800

    updated

--- wx-shortcodes.php
+++ wx-shortcodes.php
@@ -12,27 +12,27 @@ Version: 1.0
 Author URI: #
 */
 
-
 defined( 'ABSPATH' ) or die( 'No No NO!' );
 
 Class Wx_Shortcodes {
 
-	public domain = 'wx-shortcodes';
+	public $domain = 'wx-shortcodes';
+	public $plugin_path = '';
 	
 	function __construct (){
+		$this->plugin_path = dirname(__FILE__);
 		$this->init();
 	}
 
 	function init(){
 		$enabled_shortcodes = array(
-// 			'wx_buttons',
+			'wx_buttons',
 // 			'wx_buttons',
 		);
-
 		foreach ($enabled_shortcodes as $v) {
-			$f = 'shortcodes/'.$v.'.php';
-			if(file_exists($f)){
-				include $f;
+			$file_path = $this->plugin_path . '/shortcodes/'.$v.'.php';
+			if( file_exists($file_path) ){
+				include $file_path;
 			}
 		}
 	}
